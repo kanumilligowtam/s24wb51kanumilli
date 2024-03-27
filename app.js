8066d02b347dac4abe1faf5e728c3514de160d40
kanumilligowtam <kanumilligowtham@gmail.com>
2024-03-26 22:14:04 -0500

    Initial commit

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var AlcoholRouter = require('./routes/Alcohol');
 var gridRouter = require('./routes/grid');
+var pickRouter = require('./routes/pick');
 
 
 
@@ -27,6 +28,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/Alcohol', AlcoholRouter);
 app.use('/grid',gridRouter);
+app.use('/pick',pickRouter);
 
 
 
